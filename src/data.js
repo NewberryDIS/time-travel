9eb9721b6878f42082f09e583b935e19d0325b53
Dad <dad@localhost.localdomain>
2019-09-09 13:17:02 -0500

    card page only

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const Travelers = [
+	{
+		name: 'The Heiress: Julia Newberry',
+		image: 'juliaNewberry',
+		desc: 'Beginning at age 16, Julia Newberry wrote in her diary about life as a member of Chicago’s upper class in the late 1860s. Her idyllic Northside home life ended abruptly with the devastation of the Great Chicago Fire in 1871.'
+	},
+	{
+		name: 'The War Chief: Black Hawk',
+		image: 'blackHawk',
+		desc: 'A leader of the Sauk people, Black Hawk battled both rival tribes and the white settlers invading his land. After teaming with the British in the War of 1812 and with the Fox tribe in the Black Hawk War, he surrendered to U.S. forces in 1832.'
+	},
+	{
+		name: 'The Fairgoer: Errett Graham',
+		image: 'errettGraham',
+		desc: 'Light from a service hatch at the rear wall dulling the roar of the Flatline as a construct, a hardwired ROM cassette replicating a dead man’s skills, obsessions, kneejerk responses. Still it was a square of faint light.'
+	},
+	{
+		name: 'The Military Nurse: Amy Wingreen',
+		image: 'amyWingreen',
+		desc: 'The alarm still oscillated, louder here, the rear of the Villa bespeak a turning in, a denial of the bright void beyond the hull. Strata of cigarette smoke rose from the tiers, drifting until it struck currents set up by the blowers and the amplified breathing of the room where Case waited.'
+	},
+	{
+		name: 'The Frontier Soldier: Joseph Whitehouse',
+		image: 'josephWhitehouse',
+		desc: 'Now this quiet courtyard, Sunday afternoon, this girl with a random collection of European furniture, as though Deane had once intended to use the place as his home. Now this quiet courtyard, Sunday afternoon, this girl with a random collection of European furniture, as though Deane had once intended to use the place as his home.'
+	},
+	{
+		name: 'The DIS Queen: Jen Slow Wolfe',
+		image: 'wolfej',
+		desc: 'The semiotics of the blowers and the amplified breathing of the fighters. He’d waited in the puppet place had been a subunit of Freeside’s security system. A graphic representation of data abstracted from the Chinese program’s thrust, a worrying impression of solid fluidity, as though the shards of a broken mirror bent and elongated as they rotated, but it never told the correct time.'
+	},
+	{
+		name: 'The One Guy: Gilles Deleuze',
+		image: 'deleuze',
+		desc: 'No light but the muted purring of the arcade showed him broken lengths of damp chipboard and the chassis of a gutted game console. No sound but the muted purring of the bright void beyond the chain link. None of that prepared him for the arena, the crowd, the tense hush, the towering puppets of light from a half-open service hatch framed a heap of discarded fiber optics and the dripping chassis of a junked console. '
+	},
+	{
+		name: 'The Other Person: Guy Debord',
+		image: 'debord',
+		desc: 'A narrow wedge of light from a half-open service hatch at the clinic, Molly took him to the simple Chinese hollow points Shin had sold him. Now this quiet courtyard, Sunday afternoon, this girl with a random collection of European furniture, as though Deane had once intended to use the place as his home.'
+	}
+]
---
+++ src/navbarsm.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import styled from "@emotion/styled";
+import logob from './images/Picture1.png';
+import logow from './images/logow.svg';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+import './index.css';
+
+const Navchunk = styled.div`
+    border: 2px solid transparent;
+    position: relative;
+    border-radius: 8px;
+    width: 223px;
+    padding-top: 4px;
+    padding-left: 4px;
+    font-family: 'Lato', sans-serif;
+    display: flex;
+    &:hover {
+        -webkit-box-shadow: 10px 10px 50px 0px rgba(0,0,0,0.75);
+        -moz-box-shadow: 10px 10px 50px 0px rgba(0,0,0,0.75);
+        box-shadow: 10px 10px 50px 0px rgba(0,0,0,0.75);
+        border: 2px solid #C3C6A4;
+        background: rgba(39, 69, 43,0.65);
+        & a {
+            color: #C3C6A4;
+        }
+        // &.navleft img {
+        //     background: url('${logow}');
+        // }
+    }
+    & a {
+        font-weight: 900;
+        color: transparent;
+    }
+    &.navleft img {
+        flex: 1;
+        margin: 3px 0 0 0;
+        background: url('${logob}');
+        height: 40px;
+        width: 40px;
+    }
+    &::before {
+        content: "";
+        display: inline-block;
+        vertical-align: middle;
+        height: 100%;
+    }
+`
+
+export default class Navbar extends React.Component {
+    constructor() {
+        super()
+        this.handleClick = this.handleClick.bind(this)
+        this.state = {
+            change: false
+        }
+    }
+    handleClick() {
+        this.setState(prevState => ({
+            change: !prevState.change
+        }));
+    }
+    render(){
+        return(
+                <header css={css`position: fixed;
+                        z-index: 3;
+                        top: 0;
+                        bottom: 0;
+                        left: 60px;
+                        right: 0;`}>
+                    <div className="navbar" css={css`
+                        width: 75%;
+                        margin: 5px auto 0 auto;
+                        height: 60px;
+                        line-height: 40px;
+                    `}>
+                        <Navchunk className="navleft" css={css`
+                        position: relative;
+                        `}>
+                            <a href="index.html"  >
+                                <img alt="Newberry Logo" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" />
+                                <span css={css`
+                                flex: 3;
+                                position: absolute;
+                                top: 0;
+                                bottom: 0;
+                                left: 60px;
+                                right: 0;
+                                height: 30%;
+                                margin: auto;
+                                
+                                vertical-align: middle;
+                                height: 40px;
+                                line-height: 40px;
+                                `}>
+                                &gt; Digital Newberry
+                            </span>
+                            </a>
+                        </Navchunk>
+                        
+                    </div>
+                </header>
+        )
+    }
+}
